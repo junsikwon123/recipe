29da5d6cf83d5172f1ce85753054cc156f4d672e
Blaten7 <162647372+Blaten7@users.noreply.github.com>
2024-07-20 14:29:16 +0900

    관리자페이지 식자재등록 및 카테고리 수정

--- src/main/java/com/icia/recipe/management/dto/FoodItemDto.java
+++ src/main/java/com/icia/recipe/management/dto/FoodItemDto.java
@@ -15,7 +15,6 @@ public class FoodItemDto {
     private int f_num;// 글번호 오토 인크리먼트
     private String c_num;// 카테고리 대분류 참조번호
     private String c_num2; // 카테고리 중분류 참조번호
-    private String c_name;
     private String f_title; // 식자재 제목
     private String f_contents; // 식자재 내용
     private String f_price; // 식자재 가격
@@ -23,5 +22,7 @@ public class FoodItemDto {
     private String f_date; // 식자재 등록일
     private String f_edate; // 식자재 유통기한
     private String f_code; // 상품코드
-    private String f_img; // 이미지 타입은 뭐지? 일단 String은 아닌거같은데..
+    private String f_origin; // 상품 원산지
+    private String f_save; // 상품 보관 방법
+    private String f_img; // 식자재 이미지 불러올때 임시로 여기에 저장함
 }
